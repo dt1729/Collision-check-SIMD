fba5365368c716ab0ef4f697dff365d1872819d2
Divya Tiwari <divyatiwari1112@gmail.com>
2024-05-14 06:39:38 -0400

    Added collision check files

--- src/bng_lib.cpp
+++ src/bng_lib.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
+#include <vector>
 #include <x86intrin.h>
 #include <thread>
 
+struct point{
+    float _x, _y, _z;
+    public:
+        point(float x, float y, float z): _x(x), _y(y), _z(z){};
+};
+
 class abstract_polygon{
-    private:
+    protected:
         int num_edges;
+        std::vector<point> points;
 
     public:
         virtual void create_object() = 0;
-        virtual void update_properties() = 0;
+        virtual void update_properties(point p, int idx) = 0;
         virtual abstract_polygon* return_object_pointer() = 0;
         virtual abstract_polygon& operator+(abstract_polygon& p2) = 0;
+        virtual bool contains(abstract_polygon& p2) = 0;
+        virtual float volume() = 0;
 };
 
 
 class AABB : public abstract_polygon{
-    
+    private:
+        float _x_min, _y_min, _z_min;
+        float _x_max, _y_max, _z_max;
+
+    public:
+
+        AABB(point p_min, point p_max)
+            : _x_min(p_min._x), _y_min(p_min._y), _z_min(p_min._z),
+            _x_max(p_max._x), _y_max(p_max._y), _z_max(p_max._z) {
+
+                /* Serialised CW Storage of mesh's points starting from the bottom most layer
+                   in the front view for the global coordinate system.
+                */
+                for(auto i : std::vector<float>{_z_min, _z_max}){
+                    points.push_back(point(_x_min, _y_min, i));
+                    points.push_back(point(_x_max, _y_min, i));
+                    points.push_back(point(_x_max, _y_max, i));
+                    points.push_back(point(_x_min, _y_max, i));
+                }
+            }
+
+        void update_properties(point p, int idx) override{
+            this->points[idx] = std::move(p);
+        }
+
+        abstract_polygon* return_object_pointer(){
+            return this;
+        }
+
 };
