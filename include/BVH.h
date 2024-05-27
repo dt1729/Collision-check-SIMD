2ed153b020e70353459f9eba2c11335371fba6c9
Divya Tiwari <divyatiwari1112@gmail.com>
2024-05-27 06:30:16 -0400

    Refactor for parallel sort, sort along different axis

--- include/BVH.h
+++ include/BVH.h
@@ -1,41 +1,44 @@
 #include "AABB.h"
 #include "polygon.h"
 #include <algorithm>
-#include <execution>
+#include <parallel/algorithm>
 #include <vector>
 
 struct Environment{
 
     std::pair<float, float> _x_range, _y_range, _z_range;
-    std::vector<polygon> _objects;
-
-    std::vector<polygon> _x_sorted_objects = _objects;
-    std::vector<polygon> _y_sorted_objects = _objects;
-    std::vector<polygon> _z_sorted_objects = _objects;
-
-    Environment(){
-        std::sort(std::execution::par_unseq,
-                  _x_sorted_objects.begin(),
-                  _x_sorted_objects.end(),
-                  );
-
-        std::sort(std::execution::par_unseq,
-                  _y_sorted_objects.begin(),
-                  _y_sorted_objects.end());
-
-        std::sort(std::execution::par_unseq,
-                  _z_sorted_objects.begin(),
-                  _z_sorted_objects.end());
-    }
+    std::vector<polygon*> _objects;
+    Environment(std::pair<float, float> xRange, std::pair<float, float> yRange, std::pair<float, float> zRange) : 
+    _x_range(xRange), _y_range(yRange), _z_range(zRange){}
 
 };
 
 struct node{
-    std::vector<polygon> _shapes;
+    std::vector<polygon*> _objects;
     std::vector<node*> children;
+    std::vector<polygon*> _x_sorted_objects, _y_sorted_objects, _z_sorted_objects;
     float _volume;
     AABB  _region;
     bool isLeaf;
+
+    node(std::vector<polygon*> objects): _objects(objects)
+    {
+        _x_sorted_objects = _objects;
+        _y_sorted_objects = _objects;
+        _z_sorted_objects = _objects;
+
+        __gnu_parallel::sort(_x_sorted_objects.begin(),
+                             _x_sorted_objects.end(),
+                             [](polygon* p1, polygon* p2){ return p1->_x_centroid > p2->_x_centroid;});
+
+        __gnu_parallel::sort(_y_sorted_objects.begin(),
+                             _y_sorted_objects.end(),
+                             [](polygon* p1, polygon* p2){ return p1->_y_centroid > p2->_y_centroid;});
+
+        __gnu_parallel::sort(_z_sorted_objects.begin(),
+                             _z_sorted_objects.end(),
+                             [](polygon* p1, polygon* p2){ return p1->_z_centroid > p2->_z_centroid;});
+    }
 };
 
 class BVH{
@@ -47,9 +50,8 @@ class BVH{
         enum SplitAxis {X, Y, Z} _split_axis;  
 
     public:
-
         void create_BVH(){
-            
+
         }
 
 
