029ce069d38eaf73e542a0ea4498d1cc185030d1
Divya Tiwari <divyatiwari1112@gmail.com>
2024-06-21 08:57:01 -0400

    Updated data structures for maintaining objects in Environment

--- include/BVH.h
+++ include/BVH.h
@@ -1,13 +1,14 @@
-#include "AABB.h"
-#include "polygon.h"
 #include <algorithm>
 #include <parallel/algorithm>
 #include <vector>
+#include <unordered_map>
+
+#include "AABB.h"
+#include "polygon.h"
 
 struct Environment{
     public:
-        std::vector<polygon*> _objects;
-        
+        std::unordered_map<polygon*, bool> _objects;        
         std::pair<float, float> _x_range, _y_range, _z_range;
 
         Environment(std::pair<float, float> xRange, std::pair<float, float> yRange, std::pair<float, float> zRange) : 
@@ -15,11 +16,9 @@ struct Environment{
 };
 
 struct node{
-    std::vector<polygon*> _objects;
-    std::vector<polygon*> _sorted_objects;
-
+    std::unordered_map<polygon*, bool> _objects;
     std::vector<node*> children;
-    
+
     enum SplitAxis {X, Y, Z} _split_axis;
     float _x_range, _y_range, _z_range;
 
@@ -30,8 +29,6 @@ struct node{
     node(std::vector<polygon*> objects, AABB bbox): 
     _objects(objects), _region(bbox)
     {
-        _sorted_objects = _objects;
-
         if(_x_range > _y_range && _x_range > _z_range){
             _split_axis = X;
         }
@@ -42,8 +39,8 @@ struct node{
             _split_axis = Z;
         }
 
-        __gnu_parallel::sort(_sorted_objects.begin(),
-                             _sorted_objects.end(),
+        __gnu_parallel::sort(_objects.begin(),
+                             _objects.end(),
                              [this](polygon* p1, polygon* p2){ return _return_value(_split_axis, p1) > _return_value(_split_axis, p2);});
     }
 
@@ -67,26 +64,33 @@ class BVH{
         Environment env;
         std::vector<node> treeNodes;
         int branching = 4; 
+        float _threshold;
         enum SplitAxis {X, Y, Z} _split_axis;  
 
     public:
-        node* initialise_BVH(){
-
-            node* BVHNode = new node(env._objects, _split_axis);
-            return BVHNode
+        BVH(Environment _env): env(_env){
+            point p1 = point(env._x_range.first, env._y_range.first, env._z_range.first);
+            point p2 = point(env._x_range.second, env._y_range.second, env._z_range.second);
+            treeNodes.push_back(node(env._objects, AABB(p1, p2)));
         }
 
-        void create_BVH(node* root){
-
+        void create_BVH(node* root, float volume){
             /**
              * @brief Add branching nodes and call create_BVH recursively on them.
              * 
              */
+            
+            if(volume < this->_threshold){
+                return;
+            }
+
             for(int i = 0; i < branching; i++){
                 /*Create sub list of Objects
                   Create Bounding box for these objects {min(AABBs), max(AABBs)}
                  */
-                node* childNode = new node(/*Objects in split axis range take objects from sorted array in root node*/, /*AABB bounding box for the complete set of objects, so min(AABBs), max(AABBs)*/);
+                
+
+                node* childNode = new node( /*Objects in split axis range take objects from sorted array in root node*/, std::min/*AABB bounding box for the complete set of objects, so min(AABBs), max(AABBs)*/);
                 create_BVH(childNode);
                 root->children.push_back(childNode);
             }
